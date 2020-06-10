b03f9e209a99a52d5b51fe9db39e08552bf076b7
Ben Lin <cblin@andrew.cmu.edu>
2020-06-10 16:30:53 -0500

    Iterating using map and child components.

--- src/App.js
+++ src/App.js
@@ -2,66 +2,42 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from "./Movie";
+
+const movies = [
+    {
+        id: 1,
+        title: 'Star Wars'
+    },
+    {
+        id: 2,
+        title: 'Spider Man'
+    },
+    {
+        id: 3,
+        title: 'Super Man'
+    },
+    {
+        id: 4,
+        title: 'Avengers'
+    }
+];
+
 class App extends Component {
-  state = {
-    input: 'Hello',
-  };
 
-  submit = () => {
-    //
-    console.log(this.text.value)
-  };
-
-  updateInput = (event) => {
-    // This is what need to be find. onChange alwast send event.
-    // console.log(event.target.value);
-    this.setState({
-      //event.target.value.trim() will make sure no space entered
-      //event.target.value.toLowerCase() make sure no uppser case
-      input: event.target.value
-    })
-  };
-
-  render() {
-
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <Welcome text="Welcome to React!!"/>
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-
-          {/*
-          This function is DOM element of this input is referenced by this.text
-          So, when you log this.text, you can see the reference
-          */}
-          <p>{this.state.input}</p>
-          <input type="text" ref={(input) => this.text = input}/>
-          <input type="text" value={this.state.input} onChange={this.updateInput}/>
-          {/*this will run the function toggle*/}
-          <button onClick={this.submit}> Show Value</button>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </header>
+                {movies.map(movie => (
+                    <Movie key={movie.id} movie={movie}/>
+                ))}
+            </div>
+        );
+    }
 }
 
-
-function Welcome(props) {
-  const {text} = props;
-  return (
-      <h1 className="App-title"> {text} </h1>
-  )
-}
 
 export default App;
