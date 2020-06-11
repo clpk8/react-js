3f3c1b139a974bfbbc9ebc0818592202b0d612ef
Ben Lin <cblin@andrew.cmu.edu>
2020-06-10 22:41:10 -0500

    Added fetching data, changed movie to functional stateless components.

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,33 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+//Rules:
+//Should have a proptype for every single prop that have used in component
+//alwast let it either be isRequired or default props.
 
-class Movie extends Component {
-    render() {
-        return (
-            <div>
-                {this.props.movie.title}
-            </div>
-        );
-    }
-}
 
+//Functional stateless components: Anytime if you are not using states, refs, or life cycle methods.
+
+const Movie = ({movie}) => (
+    <div>
+        <h3>{movie.title} </h3>
+    </div>
+);
+
+
+//This will make sure title will be string
+//isRequired means it will be needed like (!)
+//static only works on class
+
+// static propTypes = {
+//     movie: PropTypes.shape({
+//         title: PropTypes.string.isRequired,
+//     }),
+// };
+
+//Define props types for functional stateless component
+Movie.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+    }).isRequired,
+};
 export default Movie;
